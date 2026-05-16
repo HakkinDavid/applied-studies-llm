20c09d0b1a3c5f2f766818e496d5613f407b094e
HakkinDavid <desr11@hotmail.com>
2026-05-16 03:03:38 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,13 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:4173",
-        "http://127.0.0.1:4173",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
+    allow_origins=["*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
